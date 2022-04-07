07b4e6782556692bbb8cf620837ac5c53a109f20
Husain Shahid Rao <hsr3012kalimg@gmail.com>
2022-04-08 02:30:00 +0530

    cors update

--- index.js
+++ index.js
@@ -26,20 +26,10 @@ db.sync({
   })
   .catch((err) => console.log(err));
 const app = express();
+app.use(cors());
 app.use(urlencoded({ extended: false }));
 app.use(json());
-app.use(cors());
 app.use(morgan("dev"));
-
-app.use(function(req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  next();
-});
-
-
 
 app.get("/", (req, res) => {
   return res.json({
@@ -47,8 +37,8 @@ app.get("/", (req, res) => {
   });
 });
 
-app.use("/api/user", userRoutes);
-app.use("/api/form", formRoutes);
+app.use("/api/user", cors(), userRoutes);
+app.use("/api/form", cors(), formRoutes);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log(`Server started on port ${process.env.PORT || 5000}`);
