ee300478cd53f95b69bcc55045577cec4a9e568b
Husain Shahid Rao <hsr3012kalimg@gmail.com>
2022-04-09 13:22:45 +0530

    prevent sleep on heroku

--- utils/preventSleep.js
+++ utils/preventSleep.js
@@ -5,11 +5,12 @@ const sendMail = require("../utils/sendMail");
 const preventSLeep = async () => {
   const sleepTime = process.env.SLEEP_TIME;
   if (sleepTime && sleepTime >= 60000) {
-   const refreshInterval =  setInterval(async () => {
+    const refreshInterval = setInterval(async () => {
       try {
         const resp = await axios.get(process.env.API_URL);
         if (resp.status === 200) {
           console.log("API is working");
+          await sendMail(process.env.ADMIN_EMAIL, "API is working", "API is working");
         }
       } catch (err) {
         await sendMail(process.env.ADMIN_EMAIL, "API is not working", err.message);
