febf40fcb19a961c7491137066e52bb9f848560c
Husain Shahid Rao <hsr3012kalimg@gmail.com>
2022-04-12 07:28:41 +0530

    add tests

---
+++ .jest/setEnvVars.js
@@ -0,0 +1,11 @@
+process.env.DATABASE_URL='postgresql://USER:PASSWORD@HOST:PORT/DATABASE?sslmode=verify-full'
+process.env.ADMIN_EMAIL='husainshahidrao@gmail.com'
+process.env.ADMIN_PASSWORD='admin'
+process.env.ADMIN_USER_ID='admin'
+process.env.GUEST_EMAIL='guest@email.com'
+process.env.GUEST_PASSWORD='guest@password'
+process.env.GMAIL_USER='email@gmail.com'
+process.env.GMAIL_PASSWORD='password'
+process.env.EMAIL_REMINDERS=true
+process.env.APP_SECRET='thisisasecret'
+process.env.APP_URL='https://frontend-app-url.com'
--- tests/database.test.js
+++ tests/database.test.js
@@ -1,6 +1,6 @@
-require("dotenv").config();
 const db = require("../utils/database");
 test("database", () => {
+  console.log(process.env.DATABASE_URL);
   expect(db).toBeTruthy();
   expect(db.sync).toBeTruthy();
 });
